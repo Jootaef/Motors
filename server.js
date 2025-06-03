328f1f50c5e07406a7da2bc626ec6f622747546e
jootaef <jaydanv1008@gmail.com>
2025-06-03 00:54:54 -0500

    2

--- server.js
+++ server.js
@@ -1,10 +1,4 @@
-/* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
-/* ***********************
- * Require Statements
- *************************/
+
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
@@ -18,9 +12,7 @@ const pool = require('./database/')
 const accountRoute = require('./routes/accountRoute')
 const bodyParser = require("body-parser")
 
-/* ***********************
- * Middleware
- * ************************/
+
 app.use(session({
   store: new (require('connect-pg-simple')(session))({
     createTableIfMissing: true,
@@ -33,53 +25,43 @@ app.use(session({
 }))
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true })) 
 
-// Express Messages Middleware
+
 app.use(require('connect-flash')())
 app.use(function(req, res, next){
   res.locals.messages = require('express-messages')(req, res)
   next()
 })
 
-/* ***********************
- * Routes
- *************************/
+
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout")
 
-/* ***********************
- * Routes
- *************************/
+
 app.use(express.static('public'));
 app.use(static)
 
-// Index Route
+
 app.get("/", utilities.handleErrors(baseController.buildHome))
 
-// Inventory Route
 app.use("/inv", inventoryRoute)
 
-// Account Route
 app.use('/account', accountRoute)
 
-// Trigger 500 Error Route
+
 app.get('/error', (req, res, next) => {
   const err = new Error('Testing for Error Status 500')
   err.status = 500
   next(err)
 });
 
-// File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
 
-/* ***********************
-* Express Error Handler
-* Place after all other middleware
-*************************/
+
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
@@ -92,16 +74,11 @@ app.use(async (err, req, res, next) => {
   })
 })
 
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
+
 const port = process.env.PORT || 5500
 const host = process.env.HOST || 'localhost'
 
-/* ***********************
- * Log statement to confirm server operation
- *************************/
+
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
